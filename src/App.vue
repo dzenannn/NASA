d08022fbab9d11180d90982b75dbfc64b163cd7f
dzenannn <dzenan.dev@gmail.com>
2025-02-21 04:52:34 +0100

    initial work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <WelcomeScreen />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import WelcomeScreen from './components/WelcomeScreen.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+  components: { WelcomeScreen },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #0b3d91;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="welcomeImg">
+    <img :src="welcomeData.hdurl" alt="" />
+  </div>
+  <div class="title">{{ welcomeData.title }}</div>
+  <div class="text">{{ welcomeData.explanation }}</div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeScreen',
+  created() {
+    const apiKey = process.env.VUE_APP_API_KEY;
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.welcomeData = {
+          hdurl: data.hdurl,
+          title: data.title,
+          explanation: data.explanation,
+        };
+      })
+      .catch((error) => console.error('Greška:', error));
+  },
+  data() {
+    return {
+      welcomeData: {
+        hdurl: '',
+        title: '',
+        explanation: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.welcomeImg {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+}
+
+.welcomeImg > img {
+  width: 100vw;
+  height: 100vh;
+}
+
+.title {
+  font-size: 32px;
+  position: absolute;
+  top: 20px;
+  left: 25%;
+  font-weight: 500;
+  color: #fff;
+}
+
+.text {
+  font-size: 20px;
+  position: absolute;
+  top: 50px;
+  left: 25%;
+  font-weight: 400;
+  color: #fff;
+
+  letter-spacing: 2px;
+}
+</style>
