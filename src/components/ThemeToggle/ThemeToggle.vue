3e32348c2426119ebae7cd8217ef7425c94314d9
dzenannn <dzenan.dev@gmail.com>
2025-03-08 22:48:13 +0100

    theme toggler implemented

--- src/components/ThemeToggle/ThemeToggle.vue
+++ src/components/ThemeToggle/ThemeToggle.vue
@@ -13,10 +13,17 @@ localStorage.setItem('theme', theme.value);
 
 <style scoped>
 .theme-toggle {
+  position: relative;
+  transition: bottom 1s ease 0s;
   background: none;
   border: none;
   cursor: pointer;
   font-size: 1.5rem;
   padding: 0.6rem;
+  transition: scale 0.25s;
+}
+
+.theme-toggle:hover {
+  scale: 1.3;
 }
 </style>
